15a59c610477f263a20d876cc0ca5d66e6a5304f
anantgehi <anantgehi@gmail.com>
2021-08-28 09:42:55 +0530

    Problem Statement

---
+++ Assignment no 1/src/Test.java
@@ -0,0 +1,21 @@
+/*1. Accept a integer number and when the program is executed
+print the binary,octal and hexadecimal equivalent of the given
+number.
+Sample Output:
+java Test
+Enter Number : 20
+Given Number :20
+Binary equivalent :10100
+Octal equivalent :24
+Hexadecimal equivalent :14
+Note:-Refer Java Documentation and look for the appropriate Wrapper class method
+*/
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
