1046c710d73972fdc8708ad707766b459aedd788
anantgehi <anantgehi@gmail.com>
2021-08-31 14:30:43 +0530

    assignment done

--- Assignment no 1/src/Test.java
+++ Assignment no 1/src/Test.java
@@ -11,11 +11,55 @@ Hexadecimal equivalent :14
 Note:-Refer Java Documentation and look for the appropriate Wrapper class method
 */
 
-public class Test {
+//1. Scanning an integer
+//2. Printing that integer in decimal form
+//3. Printing that integer in binary, octal and hexadecimal form
+//
+//decimal to binary -> keep dividing quotient by 2
+//rem=20%2=0
+//quo=20/2=10
+//rem=10%2=0
+//quo=10/2=5
+//rem=5%2=1
+//quo=5/2=2
+//rem=2%2=0
+//quo=2/2=1
+//rem=1%2=1
+//quo=1/2=0
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+////Using Traditional Method : 
+//import java.util.Scanner;
+//public class Test {
+//	public static void main(String[] args) {
+//		Scanner myObj = new Scanner(System.in);
+//		System.out.println("Enter a decimal no. : ");
+//		int num = myObj.nextInt();
+//		int rem, quo, bin = 0, i = 1;
+//		while (num != 0) {
+//			rem = num % 2;
+//			quo = num / 2;
+//			num = quo;
+//			bin = rem*i+bin;
+//			i *= 10;
+//		}
+//		System.out.println("Binary Conversion : ");
+//		System.out.println(bin);
+//	}
+//}
 
-	}
+//Using Wrapper Classes
+import java.util.Scanner;
 
+public class Test{
+ 
+ public static void main(String[] args) {
+ 
+  System.out.print("Enter the number :: ");
+  Scanner s = new Scanner(System.in);
+  Integer num = s.nextInt();  // Integer wrapper class
+  
+  // Use toBinaryString( ) Method of Integer wrapper class
+  String binaryNumber = Integer.toBinaryString(num); 
+  System.out.println("Binary number of " + num + " is :: "+binaryNumber);
+ }
 }
