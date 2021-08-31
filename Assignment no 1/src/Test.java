b03a60c52e2a9948b71daa3691b44bf510d64643
anantgehi <anantgehi@gmail.com>
2021-08-31 15:12:34 +0530

    Que 1 Solved

--- Assignment no 1/src/Test.java
+++ Assignment no 1/src/Test.java
@@ -27,33 +27,65 @@ Note:-Refer Java Documentation and look for the appropriate Wrapper class method
 //rem=1%2=1
 //quo=1/2=0
 
-////Using Traditional Method : 
+//Using Traditional Method : 
+//import java.util.Scanner;
+//
+//public class Test {
+//	public static void main(String[] args) {
+//		Scanner myObj = new Scanner(System.in);
+//		System.out.print("Enter the number :: ");
+//		int num = myObj.nextInt();
+//		int rem, quo, bin = 0, i = 1, num1;
+//		num1 = num;
+//		while (num1 != 0) {// true,true,true,true,true,false
+//			rem = num1 % 2;// 0,0,1,0,1
+//			quo = num1 / 2;// 10,5,2,1,0
+//			num1 = quo;// 10,5,2,1,0
+//			bin = rem * i + bin;// 0,0,100,100,10100
+//			i *= 10;// 10,100,1000,10000,100000
+//		}
+//		System.out.println("Binary number :: " + bin);// 10100
+//
+//		num1 = num;
+//		bin = 0;
+//		i = 1;
+//		while (num1 != 0) {
+//			rem = num1 % 8;
+//			quo = num1 / 8;
+//			num1 = quo;
+//			bin = rem * i + bin;
+//			i *= 10;
+//		}
+//		System.out.println("Octal number :: " + bin);
+//
+//		num1 = num;
+//		bin = 0;
+//		i = 1;
+//		while (num1 != 0) {
+//			rem = num1 % 16;
+//			quo = num1 / 16;
+//			num1 = quo;
+//			bin = rem * i + bin;
+//			i *= 10;
+//		}
+//		System.out.println("Hexadecimal number :: " + bin);
+//		myObj.close();
+//	}
+//}
+
+//Using Wrapper Classes
 import java.util.Scanner;
 public class Test {
 	public static void main(String[] args) {
-		Scanner myObj = new Scanner(System.in);
 		System.out.print("Enter the number :: ");
-		int num = myObj.nextInt();
-		int rem, quo, bin = 0, i = 1;
-		while (num != 0) {//true,true,true,true,true,false
-			rem = num % 2;//0,0,1,0,1
-			quo = num / 2;//10,5,2,1,0
-			num = quo;//10,5,2,1,0
-			bin = rem*i+bin;//0,0,100,100,10100
-			i *= 10;//10,100,1000,10000,100000
-		}
-		System.out.println("Binary number :: " + bin);//10100
+		Scanner s = new Scanner(System.in);
+		Integer num = s.nextInt();
+		String binaryNumber = Integer.toBinaryString(num);
+		System.out.println("Binary number :: " + binaryNumber);
+		String octalNumber = Integer.toOctalString(num);
+		System.out.println("Octal number :: " + octalNumber);
+		String hexNumber = Integer.toHexString(num);
+		System.out.println("Hexadecimal number :: " + hexNumber);
+		s.close();
 	}
 }
-
-////Using Wrapper Classes
-//import java.util.Scanner;
-//public class Test {
-//	public static void main(String[] args) {
-//		System.out.print("Enter the number :: ");
-//		Scanner s = new Scanner(System.in);
-//		Integer num = s.nextInt();
-//		String binaryNumber = Integer.toBinaryString(num);
-//		System.out.println("Binary number :: " + binaryNumber);
-//	}
-//}
