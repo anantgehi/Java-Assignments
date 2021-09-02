5b425be37bd421d3470018c2b8be60bcda6ab894
anantgehi <anantgehi@gmail.com>
2021-09-02 15:52:48 +0530

    Attempt 1

--- Assignments/Assignment 1/Question05/Test.java
+++ Assignments/Assignment 1/Question05/Test.java
@@ -10,3 +10,41 @@ capabilities. Create two Employee objects and display each
 object’s yearly salary. Then give each Employee a 10% raise and
 display each Employee’s yearly salary again.
 */
+
+class Employee{
+    String first_name;
+    String last_name;
+    Double salary;
+    
+    Employee(){
+        first_name= "Nil";
+        last_name = "Nil";
+        salary = 0.0;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public void setSalary(Double salary) {
+        if(salary>0)
+        this.salary = salary;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+}
