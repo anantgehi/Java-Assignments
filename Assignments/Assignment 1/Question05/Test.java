ab444f896ce2afd8c1277b221136a8ef646f0028
anantgehi <anantgehi@gmail.com>
2021-09-02 15:34:54 +0530

    Question 5 added

---
+++ Assignments/Assignment 1/Question05/Test.java
@@ -0,0 +1,12 @@
+/*
+5.Create a class called Employee that includes three instance
+variables—a first name (type String), a last name (type String)
+and a monthly salary (double). Provide a constructor that
+initializes the three instance variables. Provide a set and a
+get method for each instance variable. If the monthly salary is
+not positive, do not set its value. Write a test application
+named EmployeeTest that demonstrates class Employee’s
+capabilities. Create two Employee objects and display each
+object’s yearly salary. Then give each Employee a 10% raise and
+display each Employee’s yearly salary again.
+*/
